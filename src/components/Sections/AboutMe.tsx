439f5e54840744e1fad3450ed40d83c1faef6cb6
Daniel Gulei <daniel.ggradinaru@gmail.com>
2024-08-04 15:15:08 +0300

    fix quotes

--- src/components/Sections/AboutMe.tsx
+++ src/components/Sections/AboutMe.tsx
@@ -9,14 +9,14 @@ import LogoGroup, { LogoImageType } from '../LogoGroup';
 import { CaretDown, FileArrowDown } from '@phosphor-icons/react';
 
 const logos: LogoImageType[] = [
-  { path: "/technologies/react_logo.png", alt: "React Logo" },
-  { path: "/technologies/nodejs.png", alt: "NodeJs Logo" },
-  { path: "/technologies/tailwindcss.png", alt: "Tailwind Logo" },
-  { path: "/technologies/python.png", alt: "Python Logo" },
-  { path: "/technologies/mongo.png", alt: "MongoDB Logo" },
-  { path: "/technologies/postgresql.png", alt: "PostgreSQL Logo" },
-  { path: "/technologies/nextjs.png", alt: "NextJs Logo" },
-  { path: "/technologies/meteor.png", alt: "MeteorJs Logo" },
+  { path: '/technologies/react_logo.png', alt: 'React Logo' },
+  { path: '/technologies/nodejs.png', alt: 'NodeJs Logo' },
+  { path: '/technologies/tailwindcss.png', alt: 'Tailwind Logo' },
+  { path: '/technologies/python.png', alt: 'Python Logo' },
+  { path: '/technologies/mongo.png', alt: 'MongoDB Logo' },
+  { path: '/technologies/postgresql.png', alt: 'PostgreSQL Logo' },
+  { path: '/technologies/nextjs.png', alt: 'NextJs Logo' },
+  { path: '/technologies/meteor.png', alt: 'MeteorJs Logo' },
 ]
 
 function AboutMe() {
@@ -26,28 +26,28 @@ function AboutMe() {
   return (
     <Section idx={0} name='aboutMe'>
       <div className='card-underlay flex justify-center content-center'>
-        <div className="card section-dark flex flex-col justify-center items-center gap-12">
-        <div className="top-row flex justify-center items-center gap-16">
+        <div className='card section-dark flex flex-col justify-center items-center gap-12'>
+        <div className='top-row flex justify-center items-center gap-16'>
           <img
-            className="image w-64 aspect-square object-top"
-            src="/about_me_image.jpg"
-            alt="TODO: add some alt text here"
+            className='image w-64 aspect-square objectobject-top'
+            src='/about_me_image.jpg'
+            alt='TODO: add some alt text here'
           />
           <div>
-            <div className="mb-3 text-5xl">Hi there!</div>
-            <div className="text-2xl">
+            <div className='mb-3 text-5xl'>Hi there!</div>
+            <div className='text-2xl'>
               I'm a junior fullstack developer specialising in React, NodeJs and
               MongoDB with ~1 year of professional experience.
             </div>
           </div>
         </div>
-        <div className="text-center text-3xl grow-[1] flex flex-col gap-2">
+        <div className='text-center text-3xl grow-[1] flex flex-col gap-2'>
           <div className='z-[2]'>
             Based in Romania, passioned by art and <span>electronic</span>{' '}
             music.
             <br />I work fairly often with technologies such as:
           </div>
-          <div className="flex gap-4 justify-center">
+          <div className='flex gap-4 justify-center'>
             <LogoGroup images={[logos[0], logos[1]]} offset={0} />
             <LogoGroup images={[logos[2], logos[3]]} offset={1.2} />
             <LogoGroup images={[logos[4], logos[5]]} offset={2.45} />
@@ -56,7 +56,7 @@ function AboutMe() {
           <div>
             Welcome to my portfolio website!
           </div>
-          <div className="bottom-row flex justify-center items-center gap-16">
+          <div className='bottom-row flex justify-center items-center gap-16'>
             <a
               href='@assets/CV_Gulei_Daniel.pdf'
               download='CV_Gulei_Daniel.pdf'
@@ -70,7 +70,7 @@ function AboutMe() {
               </animated.span>
             </a>
             <Link
-              to="work"
+              to='work'
               spy={true}
               smooth={true}
               duration={SCROLL_SPEED * 1.5}
